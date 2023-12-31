406bd9ea5b70886214840942079468450eb85759
R2turnTrue <46389044+R2turnTrue@users.noreply.github.com>
2023-12-31 11:50:50 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Kotlin Paper Template
+Template For PaperMC (Kotlin)
+
+You can use this Template
---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    id("io.papermc.paperweight.userdev") version "1.5.11"
+}
+
+group = "xyz.r2turntrue"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    paperweight.paperDevBundle("1.20.2-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
+}
+
+
+//val shade = configurations.create("shade")
+//shade.extendsFrom(configurations.implementation.get())
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+tasks {
+
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+
+    compileKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+    
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+    
+    processResources {
+        filesMatching("*.yml") {
+            expand(project.properties)
+        }
+    }
+    
+    create<Jar>("sourceJar") {
+        archiveClassifier.set("source")
+        from(sourceSets["main"].allSource)
+    }
+
+    jar {
+        //from (shade.map { if (it.isDirectory) it else zipTree(it) })
+    }
+
+    assemble {
+        dependsOn(reobfJar)
+    }
+}
+
